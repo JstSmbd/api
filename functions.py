3aa5718a1d22c1d10fc9cbf27e506d2fe862a9b2
JstSmbd <arsengoodvin@yandex.ru>
2023-02-18 14:07:33 +0300

    К задаче "Угадай-ка город"

--- functions.py
+++ functions.py
@@ -1,4 +1,6 @@
 import math
+import requests
+from random import uniform
 
 
 def find_bbox(toponym):
@@ -8,7 +10,6 @@ def find_bbox(toponym):
 
 
 def lonlat_distance(a, b):
-
     degree_to_meters_factor = 111 * 1000
     a_lon, a_lat = a
     b_lon, b_lat = b
@@ -22,3 +23,35 @@ def lonlat_distance(a, b):
     distance = math.sqrt(dx * dx + dy * dy)
 
     return distance
+
+
+def get_coords(geocode, bbox=False):
+    answer = requests.get("http://geocode-maps.yandex.ru/1.x/", params={
+        "apikey": "40d1649f-0493-4b70-98ba-98533de7710b",
+        "geocode": geocode,
+        "format": "json"}).json()["response"][
+        "GeoObjectCollection"]["featureMember"][0]["GeoObject"]
+    return find_bbox(answer) if bbox else answer["Point"]["pos"].replace(" ", ",")
+
+
+def get_map(params):
+    return requests.get("http://static-maps.yandex.ru/1.x/", params=params)
+
+
+def get_bbox_part(bbox, type):
+    list_bbox = [list(map(float, (list_bbox := bbox.split("~"))[0].split(","))),
+                 list(map(float, list_bbox[1].split(",")))]
+    sizes = [abs(list_bbox[0][0] - list_bbox[1][0]), abs(list_bbox[0][1] - list_bbox[1][1])]
+    if type == "map":
+        pr = uniform(0.005 / sizes[0], 0.032 / sizes[0])
+        # чтобы не было видно названия городов
+    else:
+        pr = uniform(0.005 / sizes[0], 1)
+    next_sizes = [size * pr for size in sizes]
+    lw_corner = [list_bbox[0][0] + uniform(0, sizes[0] - next_sizes[0]),
+                 list_bbox[0][1] + uniform(0, sizes[1] - next_sizes[1])]
+    return f"{lw_corner[0]},{lw_corner[1]}~" \
+           f"{lw_corner[0] + next_sizes[0]}," \
+           f"{lw_corner[1] + next_sizes[1]}"
+    # вроде бы, должно работать
+
--- main.py
+++ main.py
@@ -1,26 +1,64 @@
 import requests
 import sys
-from io import BytesIO
-from PIL import Image, UnidentifiedImageError
-from functions import lonlat_distance
+import os
+from random import choice
+from PyQt5.QtGui import QPixmap
+from PyQt5.QtWidgets import QApplication, QWidget, QLabel
+from PyQt5.QtCore import Qt
+from functions import get_coords, get_map, get_bbox_part
 
-try:
-    address_ll = requests.get("http://geocode-maps.yandex.ru/1.x/", params={
-        "apikey": "40d1649f-0493-4b70-98ba-98533de7710b",
-        "geocode": " ".join(sys.argv[1:]),
-        "format": "json"}).json()["response"][
-        "GeoObjectCollection"]["featureMember"][0]["GeoObject"]["Point"]["pos"].replace(" ", ",")
+cities = ["Димитровград", "Санкт-Петербург", "Москва", "Красноярск", "Новосибирск",
+          "Екатеринбург", "Казань", "Нижний Новгород", "Челябинск", "Самара", "Уфа",
+          "Ростов-на-Дону", "Омск", "Краснодар", "Воронеж", "Пермь", "Волгоград", "Саратов",
+          "Тюмень", "Тольятти"]
 
-    response = requests.get("http://geocode-maps.yandex.ru/1.x/", params={
-        "apikey": "40d1649f-0493-4b70-98ba-98533de7710b",
-        "kind": "district",
-        "geocode": address_ll,
-        "format": "json"
-    }).json()["response"]["GeoObjectCollection"]["featureMember"][0]["GeoObject"][
-        "metaDataProperty"]["GeocoderMetaData"]["Address"]["Components"]
+SCREEN_SIZE = [600, 450]
 
-    district = " или ".join([kind["name"] for kind in response
-                             if kind["kind"] == "district" and "район" in kind["name"].lower()])
-    print(district if district else "нет района")
-except (IndexError, KeyError):
-    print("bad parameters")
+
+class Example(QWidget):
+    def __init__(self):
+        super().__init__()
+        self.map_file = "map.png"
+        self.initUI()
+
+    def changeImage(self):
+        city = choice(cities)
+        type = choice(["map", "sat"])
+        response = get_map({
+            "bbox": get_bbox_part(get_coords(city, bbox=True), type),
+            "l": type
+        })
+
+        with open(self.map_file, "wb") as file:
+            file.write(response.content)
+
+        self.image.setPixmap(QPixmap(self.map_file))
+
+    def initUI(self):
+        self.setFixedSize(*SCREEN_SIZE)
+        self.setWindowTitle('Угадай-ка город')
+
+        self.image = QLabel(self)
+        self.image.move(0, 0)
+        self.image.resize(*SCREEN_SIZE)
+
+        self.changeImage()
+
+    def mousePressEvent(self, event):
+        if event.button() in (1, 2):
+            self.changeImage()
+
+    def keyPressEvent(self, event):
+        if event.key() == Qt.Key_Space:
+            self.changeImage()
+
+    def closeEvent(self, event):
+        """При закрытии формы подчищаем за собой"""
+        os.remove(self.map_file)
+
+
+if __name__ == '__main__':
+    app = QApplication(sys.argv)
+    ex = Example()
+    ex.show()
+    sys.exit(app.exec())
