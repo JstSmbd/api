b3e7ce17247dc5d23849252543ece90ed2edcaea
JstSmbd <arsengoodvin@yandex.ru>
2023-02-20 19:05:01 +0300

    К задаче "Длина пути"

--- main.py
+++ main.py
@@ -1,64 +1,36 @@
 import requests
 import sys
-import os
-from random import choice
-from PyQt5.QtGui import QPixmap
-from PyQt5.QtWidgets import QApplication, QWidget, QLabel
-from PyQt5.QtCore import Qt
-from functions import get_coords, get_map, get_bbox_part
+from io import BytesIO
+from PIL import Image, UnidentifiedImageError
+from functions import request_point, lonlat_distance, find_middle_line
 
-cities = ["Димитровград", "Санкт-Петербург", "Москва", "Красноярск", "Новосибирск",
-          "Екатеринбург", "Казань", "Нижний Новгород", "Челябинск", "Самара", "Уфа",
-          "Ростов-на-Дону", "Омск", "Краснодар", "Воронеж", "Пермь", "Волгоград", "Саратов",
-          "Тюмень", "Тольятти"]
+coords = [request_point("Санкт-Петербург, Петергоф"), [30.044691, 59.912339],
+          [30.265166, 59.917148], [30.275479, 59.929408], [30.282346, 59.932818],
+          request_point("Санкт-Петербург, Эрмитажная пристань"), [30.308389, 59.946044],
+          [30.285011, 59.949008], [30.264646, 59.956940], [30.196755, 59.966286],
+          request_point("Санкт-Петербург, Петергоф")]
 
-SCREEN_SIZE = [600, 450]
+if len(coords) > 1:
+    distances = []
+    for i, coord in enumerate(coords[:-1]):
+        distances.append(lonlat_distance(coord, coords[i + 1]))
+    i, procent = find_middle_line(sum(distances) / 2, distances)
+    point1, point2 = coords[i], coords[i + 1]
+    answers = f"{point1[0] - (point1[0] - point2[0]) * procent}," \
+              f"{point1[1] - (point1[1] - point2[1]) * procent}"
+elif len(coords) == 1:
+    answers = f"{coords[0][0]},{coords[0][1]}"
+    distances = [0]
+else:
+    print(f"Длина: 0m")
+    sys.exit()
 
+response = requests.get("http://static-maps.yandex.ru/1.x/", params={
+    "l": "map",
+    "pl": ",".join([f"{coord[0]},{coord[1]}" for coord in coords]) if len(coords) > 1 else "",
+    "pt": f"{answers},pm2rdm"
+})
 
-class Example(QWidget):
-    def __init__(self):
-        super().__init__()
-        self.map_file = "map.png"
-        self.initUI()
+print(f"Длина: {sum(distances)}m")
 
-    def changeImage(self):
-        city = choice(cities)
-        type = choice(["map", "sat"])
-        response = get_map({
-            "bbox": get_bbox_part(get_coords(city, bbox=True), type),
-            "l": type
-        })
-
-        with open(self.map_file, "wb") as file:
-            file.write(response.content)
-
-        self.image.setPixmap(QPixmap(self.map_file))
-
-    def initUI(self):
-        self.setFixedSize(*SCREEN_SIZE)
-        self.setWindowTitle('Угадай-ка город')
-
-        self.image = QLabel(self)
-        self.image.move(0, 0)
-        self.image.resize(*SCREEN_SIZE)
-
-        self.changeImage()
-
-    def mousePressEvent(self, event):
-        if event.button() in (1, 2):
-            self.changeImage()
-
-    def keyPressEvent(self, event):
-        if event.key() == Qt.Key_Space:
-            self.changeImage()
-
-    def closeEvent(self, event):
-        """При закрытии формы подчищаем за собой"""
-        os.remove(self.map_file)
-
-
-if __name__ == '__main__':
-    app = QApplication(sys.argv)
-    ex = Example()
-    ex.show()
-    sys.exit(app.exec())
+Image.open(BytesIO(response.content)).show()
